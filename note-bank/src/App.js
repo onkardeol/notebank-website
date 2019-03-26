1a5f81acffca854834853b66f7670cd3355f36ef
onkardeol <onkardeol7@gmail.com>
2019-03-25 21:50:42 -0400

    Landing page commit

--- note-bank/src/App.js
+++ note-bank/src/App.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './materialize/css/materialize.css';
+import Background from './images/background.jpg'
+
+const cardPStyle = {
+  marginLeft:'10px',
+  display:'inline-block'
+}
+
+const backgroundImage = {
+  backgroundImage: "url(" + Background + ")",
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  minHeight: '100%'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="App" style={backgroundImage}>
+
+        <div className="valign-wrapper row-landingpage login-box">
+          <div className="col card hoverable s10 pull-s1 m6 pull-m3 l4 pull-l4">
+            <form>
+              <div className="card-content">
+                <span className="card-title">Enter credentials</span>
+                
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input type="email" className="validate" name="email" id="email" placeholder="Email address"/>
+                  </div>
+                  
+                  <div className="input-field col s12">
+                    <input type="password" className="validate" name="password" id="password" placeholder="Password"/>
+                  </div>
+                </div>
+              </div>
+              
+
+                <div>
+                  <div className="card-action right-align">
+                      <p className="margin medium-small" style={cardPStyle}><a href="#">Register</a></p>
+                      <button className="btn waves-effect waves-light" type="submit" name="action">Sign  In</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
     );
   }
 }
